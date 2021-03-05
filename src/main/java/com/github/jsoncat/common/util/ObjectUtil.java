abc7e1c0ceb00f95ffeb75679dcd71bf07ad3c8a
xushuanglong <1872744675@qq.com>
2021-03-05 16:42:27 +0800

    build

---
+++ src/main/java/com/github/jsoncat/common/util/ObjectUtil.java
@@ -0,0 +1,24 @@
+package com.github.jsoncat.common.util;
+
+import java.beans.PropertyEditor;
+import java.beans.PropertyEditorManager;
+
+/**
+ * @Description 属性编辑器
+ * @Author Goodenough
+ * @Date 2021/3/5 15:46
+ */
+public class ObjectUtil {
+
+    /**
+     * 将字符串转换为目标类型
+     * @param targetType 要转换的目标类型
+     * @param s 要转换的字符串
+     * @throws NumberFormatException When string to number, if string is not a number,then throw NumberFormatException
+     */
+    public static Object convert(Class<?> targetType, String s){
+        PropertyEditor editor = PropertyEditorManager.findEditor(targetType);
+        editor.setAsText(s);
+        return editor.getValue();
+    }
+}
---
+++ src/main/java/com/github/jsoncat/common/util/ReflectionUtil.java
@@ -0,0 +1,107 @@
+package com.github.jsoncat.common.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.reflections.Reflections;
+import org.reflections.scanners.TypeAnnotationsScanner;
+
+import javax.validation.ConstraintViolationException;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Set;
+
+/**
+ * @Description 反射通用方法
+ * @Author Goodenough
+ * @Date 2021/3/5 15:57
+ */
+@Slf4j
+public class ReflectionUtil {
+
+    /**
+     * 扫描指定包下有指定注解的类
+     * @param packageNames 指定的包名
+     * @param annotation 指定的注解
+     * @return 指定包下标记了指定注解的类的 class 对象集合
+     */
+    public static Set<Class<?>> scanAnnotationClass(String[] packageNames, Class<? extends Annotation> annotation){
+        Reflections reflections = new Reflections(packageNames, new TypeAnnotationsScanner());
+        Set<Class<?>> annotationClass = reflections.getTypesAnnotatedWith(annotation, true);
+        log.info("The number of class Annotated with @"+annotation.getSimpleName()+":[{}]", annotationClass.size());
+        return annotationClass;
+    }
+
+    /**
+     * 获得指定接口的实现类
+     * @param packageNames 指定包名
+     * @param interfaceClass 指定接口
+     * @return 实现了指定接口的子类集合
+     */
+    public static <T> Set<Class<? extends T>> getSubClass(Object[] packageNames, Class<T> interfaceClass){
+        Reflections reflections = new Reflections(packageNames);
+        return reflections.getSubTypesOf(interfaceClass);
+    }
+
+    /**
+     * 通过类型创建实例对象
+     * @param cls 目标类型
+     * @return 通过目标类型创建的实例对象
+     */
+    public static Object newInstance(Class<?> cls){
+        try {
+            return cls.newInstance();
+        } catch (InstantiationException | IllegalAccessException e) {
+            throw new IllegalStateException(e.getMessage(), e);
+        }
+    }
+
+    /**
+     * 设置指定对象中指定域的值
+     * @param obj 目标对象
+     * @param field 目标字段
+     * @param value 要设置的值
+     */
+    public static void setField(Object obj, Field field, Object value){
+        field.setAccessible(true);
+        try {
+            field.set(obj, value);
+        } catch (IllegalAccessException impossible) {
+            throw new AssertionError(impossible);
+        }
+    }
+
+    /**
+     * 执行指定方法
+     * @param targetObject 方法所属对象
+     * @param method 目标方法
+     * @param args 方法的参数
+     * @return 方法执行结果
+     */
+    public static Object executeTargetMethod(Object targetObject, Method method, Object... args){
+        try {
+            return method.invoke(targetObject, args);
+        } catch (Throwable t) {
+            if (t.getCause() != null && t.getCause() instanceof ConstraintViolationException) {
+                throw (ConstraintViolationException) t.getCause();
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 执行指定的无返回值的方法
+     * @param targetObject 方法所属对象
+     * @param method 目标方法
+     * @param args 方法所需参数
+     */
+    public static void executeTargetMethodNoResult(Object targetObject, Method method, Object... args){
+        try {
+            method.invoke(targetObject, args);
+        } catch (Throwable t) {
+            if (t.getCause() != null && t.getCause() instanceof ConstraintViolationException) {
+                throw (ConstraintViolationException) t.getCause();
+            }
+        }
+    }
+}
